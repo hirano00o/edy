8f0ddfc8d74ccd7064552ef2c9dcac18208a5a27
Yudai Hiranuma <uday6827@gmail.com>
2021-05-15 01:04:24 +0900

    Modify to work on github actions

--- test/run.sh
+++ test/run.sh
@@ -2,15 +2,7 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-setup () {
-  echo "Setup integration test."
-  echo ""
-  docker run --rm --name integration_test -d -p 8000:8000 amazon/dynamodb-local >/dev/null
-  while ! nc localhost 8000 -v -w 1 > /dev/null
-  do
-    sleep 1
-  done
-
+initialise() {
   if ! aws dynamodb create-table \
     --region ap-northeast-1 \
     --table-name User \
@@ -39,15 +31,7 @@ setup () {
   go build -o "${SCRIPT_DIR}"/edy "${SCRIPT_DIR}"/../cmd/edy/main.go
 }
 
-tearDown () {
-  rm -f "${SCRIPT_DIR}"/edy "${SCRIPT_DIR}"/cases/*.json
-  docker stop integration_test >/dev/null
-  echo ""
-  echo "Finished integration test."
-}
-
-trap tearDown 0 1 2 3 15
-setup
+initialise
 
 for f in "${SCRIPT_DIR}"/cases/*.sh
 do
